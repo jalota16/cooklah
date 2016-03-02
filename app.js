cc9b00ada97a03878a5f5193c8787b2ecb2f564c
rotarycut <shermanyong@gmail.com>
2016-03-03 01:21:47 +0800

    drag and drop

---
+++ app.js
@@ -0,0 +1,39 @@
+Sortable.create(number1, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(number2, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(number3, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(number4, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(number5, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(number6, {
+    group: {name:"DragFood", pull:'clone', put:false},
+    animation: 150,
+})
+
+Sortable.create(cart1, {
+    group: {name:"DragFood", put:true, pull:false},
+    animation: 150,
+})
+
+Sortable.create(cart2, {
+    group: {name:"DragFood", put:true, pull:false},
+    animation: 150,
+})
